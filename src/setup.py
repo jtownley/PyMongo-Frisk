c1e175e20ca02c27beece4727b08870bc57a6849
James Townley <jtownley@jtownley.point2.com>
2010-07-15 11:04:55 -0600

    Change version to be sane

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 setup(name='pymongo_frisk',
-      version='0.1.1',
+      version='0.0.1',
       description='Wrapper for PyMongo Connection that offers additional application level monitoring. When connecting to multiple MongoDB servers, allows you to verify connectivity to MongoDB slave servers.',
       author='Brian Richardson , James Townley',
       url='http://www.point2.com/',
